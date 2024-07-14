7298d2df9f5b13ac98b51bd1a79bcbd4abb35244
Vaibhav Shelar <vaibhavshelar269@gmail.com>
2024-07-14 19:29:09 +0530

    feature request1 from challenge 2

---
+++ day_2/day_2_p1.js
@@ -0,0 +1,24 @@
+// Challenge 2 : https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3197073
+
+// This code file will only contain feature request 1 i.e have tasks one to seven
+
+// Activity 1
+
+let num1=55
+let num2=44
+
+console.log(`The sum of ${num1} and ${num2} is ${num1+num2}`);
+console.log(`The difference of ${num1} and ${num2} is ${num1-num2}`);
+console.log(`The product of ${num1} and ${num2} is ${num1*num2}`);
+console.log(`The division of ${num1} and ${num2} is ${num1/num2}`);
+console.log(`The remainder when ${num1} is divided by ${num2} is ${num1%num2}`);
+
+// Activity 2
+
+console.log(`${num2} when added with 11 is:`);
+num2+=11
+console.log(num2);
+
+console.log(`${num1} when substracted with 11 is:`);
+num1-=11
+console.log(num1);
