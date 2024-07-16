cd2c6b02e0343c0847439aa4561f02633ba3ab91
Vaibhav Shelar <vaibhavshelar269@gmail.com>
2024-07-16 16:56:48 +0530

    feature request 1

---
+++ day_4/day_4_p1.js
@@ -0,0 +1,22 @@
+// Challenge 4 : https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3197093
+
+// This code file will only contain feature request 1 i.e. task 1 and task 4
+
+// Task 1
+
+console.log("The counting from 1-10 goes as :");
+
+for (let index = 1; index <= 10; index++) {
+    console.log(index);
+    
+}
+
+// Task 4
+
+console.log("Now let's see the counting in reverse order");
+
+let revIndex=10
+while (revIndex>=1) {
+    console.log(revIndex);
+    revIndex=revIndex-1
+}
