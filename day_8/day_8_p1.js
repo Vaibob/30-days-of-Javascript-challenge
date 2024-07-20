2fdb04ae2368025364c1ea5a666e7f1cd4fb1d11
Vaibhav Shelar <vaibhavshelar269@gmail.com>
2024-07-20 10:44:25 +0530

    feature request 1

---
+++ day_8/day_8_p1.js
@@ -0,0 +1,24 @@
+// Challenge 8 : https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3197148
+
+// This code file will only contain feature request 1 i.e. tasks 1 , 2
+
+// Task 1 and 2
+
+const name = "Alice";
+const age = 30;
+const city = "Wonderland";
+
+const message = `Hello, my name is ${name}.
+I am ${age} years old.
+I live in ${city}.
+
+Nice to meet you!`;
+
+console.log(message);
+
+// Hello, my name is Alice.
+// I am 30 years old.
+// I live in Wonderland.
+
+// Nice to meet you!
+
