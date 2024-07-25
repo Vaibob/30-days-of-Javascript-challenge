ccf3d274f71e2144fb36627e29550cb69b3ca5e2
Vaibhav Shelar <vaibhavshelar269@gmail.com>
2024-07-26 00:08:39 +0530

    feature request 5

---
+++ day_13/src/day_13_p5.mjs
@@ -0,0 +1,11 @@
+// Challenge 13 : https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3197208
+
+// This code file will only contain feature request 4 i.e. tasks 8
+
+// Task 8
+
+import { greet } from './module1.mjs';
+import { add } from './module2.mjs';
+
+console.log(greet('World')); // Output: Hello, World
+console.log(add(2, 3)); // Output: 5
